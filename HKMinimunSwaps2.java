fc44d710d7d56226b07fea574c38972959b0717a
phande-dev <83936215+phande-dev@users.noreply.github.com>
2021-06-06 20:55:07 +0530

    Add files via upload

---
+++ HKMinimunSwaps2.java
@@ -0,0 +1,62 @@
+package test;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+         int l=0;
+         int r=arr.length;
+         System.out.println(r);
+         int swap=0;
+         while(l<r){
+             if(arr[l]!=(l+1)){
+                int temp=arr[l];
+              //  arr[l]=l+1;
+              arr[l] = arr[temp-1];
+                arr[temp-1]=temp;
+                swap++; 
+             }else
+             l++;
+         }
+         System.out.println(Arrays.toString(arr));
+         return swap;
+       
+         
+    }
+    
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int res = minimumSwaps(arr);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
